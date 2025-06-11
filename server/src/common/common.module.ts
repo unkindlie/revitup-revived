2acdbadf20d2ec6ef76025bdbb6546aea1bae0db
unkindlie <inoutoftheverge@gmail.com>
2025-06-11 19:41:34 +0300

    feat: typeorm setup

--- server/src/common/common.module.ts
+++ server/src/common/common.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { DatabaseModule } from './database/database.module';
+import databaseConfig from './database/database.config';
+
 @Module({
     imports: [
-        ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
+        ConfigModule.forRoot({
+            envFilePath: `.env.${process.env.NODE_ENV}`,
+            load: [databaseConfig],
+        }),
+        DatabaseModule,
     ],
 })
 export class CommonModule {}
---
+++ server/src/common/database/database.config.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+    port: process.env.PG_DB_PORT,
+    username: process.env.PG_DB_USER,
+    password: process.env.PG_DB_PASSWORD,
+    database: process.env.PG_DB_NAME,
+}));
