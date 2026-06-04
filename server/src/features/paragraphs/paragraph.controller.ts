b56ebda108637d3ae3c93223c5d0c79972db7756
Taras Blyzniuk <inoutoftheverge@gmail.com>
2026-06-04 18:59:01 +0300

    feat(paragraphs): other crud endpoints

--- server/src/features/paragraphs/paragraph.controller.ts
+++ server/src/features/paragraphs/paragraph.controller.ts
@@ -1,12 +1,55 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
 import { ParagraphService } from './paragraph.service';
+import { ParagraphCreateDto } from './dto/paragraph-create.dto';
+import { ParagraphUpdateDto } from './dto/paragraph-update.dto';
 
 @Controller('paragraphs')
 export class ParagraphController {
-  constructor(private service: ParagraphService) {}
+  constructor(private readonly service: ParagraphService) {}
 
   @Get('article/:articleId')
-  findByArticle(@Param('articleId', ParseIntPipe) articleId: number) {
+  async findByArticle(@Param('articleId', ParseIntPipe) articleId: number) {
     return this.service.findByArticle(articleId);
+  }
+
+  @Post()
+  async create(@Body() dto: ParagraphCreateDto) {
+    await this.service.create(dto);
+
+    return { message: 'Paragraph created successfully' };
+  }
+
+  @Post('bulk')
+  async createMany(@Body() dtoList: ParagraphCreateDto[]) {
+    await this.service.createMany(dtoList);
+
+    return { message: 'Paragraphs created successfully' };
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: ParagraphUpdateDto,
+  ) {
+    await this.service.update(id, dto);
+
+    return { message: 'Paragraph updated successfully' };
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    await this.service.delete(id);
+
+    return { message: 'Paragraph deleted successfully' };
   }
 }
