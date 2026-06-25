f5150ac7deaf222ad3dd439f8515be488fa907a7
Taras Blyzniuk <inoutoftheverge@gmail.com>
2026-06-25 17:29:17 +0300

    fix(header): profile img loading state

--- client/src/components/container/header/HeaderProfile.tsx
+++ client/src/components/container/header/HeaderProfile.tsx
@@ -5,30 +5,42 @@ import { useGetUserLatestPfp } from '@/hooks/features/users/useGetUserLatestPfp'
 import { useResponse } from '@/hooks/useResponse';
 import { Pages, path } from '@/lib/routing/client';
 import { useUserStore } from '@/stores/user.store';
+import { useState } from 'react';
+import { Spinner } from '../../common/spinner/Spinner';
 
 export const HeaderProfile = () => {
   const isLogged = useUserStore((s) => s.isLogged);
   const user = useUserStore((s) => s.user);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  const { data: pfpSrcRes } = useGetUserLatestPfp(user?.id || 0);
+  const { data: pfpSrcRes, isFetched } = useGetUserLatestPfp(user?.id || 0);
   const { data: pfpSrc } = useResponse(pfpSrcRes);
 
   if (!isLogged) return null;
 
-  if (!pfpSrc) {
+  if (isFetched && !pfpSrc) {
     return (
       <a href={path(Pages.Profile)} target="_blank" rel="noopener noreferrer">
-        <RevitupLogo className="h-9 w-9 cursor-pointer fill-white object-cover" />
+        <RevitupLogo className="size-9 cursor-pointer fill-white object-cover" />
       </a>
     );
   }
 
   return (
-    <Link to={path(Pages.Profile)}>
+    <Link to={path(Pages.Profile)} className="relative block size-9">
+      {!isLoaded && (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <Spinner size="sm" />
+        </div>
+      )}
+
       <img
-        src={pfpSrc}
+        src={pfpSrc || undefined}
         alt="profile"
-        className="size-9 min-h-9 min-w-9 cursor-pointer rounded-sm object-cover"
+        onLoad={() => setIsLoaded(true)}
+        className={`size-9 min-h-9 min-w-9 rounded-sm object-cover ${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        }`}
       />
     </Link>
   );
